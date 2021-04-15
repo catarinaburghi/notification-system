409483cffc73572e404d77208927662c5e9fd83f
Catarina <cf.burghi@gmail.com>
2021-04-15 10:55:13 -0400

    comments and changes

--- notifications.java
+++ notifications.java
@@ -1,18 +1,22 @@
 package notifications;
-
+/*This program is just to show 
+*how a simple notification would
+*appear when it notices a unauthorized
+*entry to the system.
+*/
 import java.util.Scanner;
 
 public class notifications{
 public static void main(String[] args) {
     
     Scanner sc = new Scanner(System.in);
-    Integer words;
+    double choice;
 
     System.out.println("Would you like to try and enter the system...");
     System.out.println("Enter 1 for yes, 2 for no");
-    words = sc.nextInt();
+    choice = sc.nextDouble();
 
-    if (words == 1){
+    if (choice == 1){
         System.out.println("An unothorized entry has been detected...");
     } else {
         System.out.println("System is secured");
